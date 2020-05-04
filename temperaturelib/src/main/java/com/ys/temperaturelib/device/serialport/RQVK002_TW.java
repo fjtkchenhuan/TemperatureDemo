6bf6c2cc4e9398bc04c40efd39029661f7de4e7b
sky <13714854975@163.com>
2020-05-04 17:06:42 +0800

    RQVK002-TWBD-DS(台湾宝岛)添加补偿值

--- temperaturelib/src/main/java/com/ys/temperaturelib/device/serialport/RQVK002_TW.java
+++ temperaturelib/src/main/java/com/ys/temperaturelib/device/serialport/RQVK002_TW.java
@@ -11,7 +11,7 @@ import com.ys.temperaturelib.utils.DataFormatUtil;
 import java.util.Arrays;
 
 public class RQVK002_TW extends ProductImp implements TemperatureParser<byte[]> {
-    public static final String DEFAULT_MODE_NAME = "RQVK002-TW(单点)"; //型号
+    public static final String DEFAULT_MODE_NAME = "RQVK002-TWBD-DS(台湾宝岛)"; //型号
     static final String DEFAULT_DEVICE = "/dev/ttyS3"; //设备号
     static final int DEFAULT_RATE = 19200; //波特率
 
@@ -19,6 +19,7 @@ public class RQVK002_TW extends ProductImp implements TemperatureParser<byte[]> 
         super(DEFAULT_DEVICE, DEFAULT_RATE
                 , new MeasureParm(DEFAULT_MODE_NAME, 0, 500, 0, 0));
         setTemperatureParser(this);
+        setTakeTempEntity(getDefaultTakeTempEntities()[0]);
     }
 
 
@@ -38,8 +39,35 @@ public class RQVK002_TW extends ProductImp implements TemperatureParser<byte[]> 
     }
 
     @Override
+    public TakeTempEntity[] getDefaultTakeTempEntities() {
+        TakeTempEntity[] entities = new TakeTempEntity[4];
+        TakeTempEntity entity0 = new TakeTempEntity();
+        entity0.setDistances(20);
+        entity0.setTakeTemperature(-0.4f);//-0.4
+        entities[0] = entity0;
+
+        TakeTempEntity entity1 = new TakeTempEntity();
+        entity1.setDistances(30);
+        entity1.setTakeTemperature(0.1f);//-0.1
+        entities[1] = entity1;
+
+        TakeTempEntity entity2 = new TakeTempEntity();
+        entity2.setDistances(40);
+        entity2.setTakeTemperature(0.2f);//0.2
+        entities[2] = entity2;
+
+        TakeTempEntity entity3 = new TakeTempEntity();
+        entity3.setDistances(50);
+        entity3.setTakeTemperature(0.6f);//0.6
+        entities[3] = entity3;
+        return entities;
+    }
+
+    @Override
     public float check(float value, float ta) {
-        return value;
+        TakeTempEntity takeTempEntity = getTakeTempEntity();
+        if (!takeTempEntity.isNeedCheck()) return value;
+        return value + takeTempEntity.getTakeTemperature();
     }
 
     @Override
